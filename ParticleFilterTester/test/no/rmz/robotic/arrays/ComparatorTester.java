7beb5f14bdfe859d4e3abdc8d53031c41a8f2d25
Bjorn Remseth <la3lma@gmail.com>
2012-01-15 21:53:12 +0100

    Make the integer tester available for other tests

--- ParticleFilterTester/test/no/rmz/robotic/arrays/ComparatorTester.java
+++ ParticleFilterTester/test/no/rmz/robotic/arrays/ComparatorTester.java
@@ -21,24 +21,19 @@ import org.junit.*;
 
 public class ComparatorTester {
  
-    Comparator<Integer> comparator;
+    public final static Comparator<Integer> INTEGER_COMPARATOR = new Comparator<Integer>() {
+
+        @Override
+        public int compare(final Integer t, final Integer t1) {
+            return t.compareTo(t1);
+        }
+    };
     Integer i1 = 1;
     Integer i2 = 2;
-    @Before
-    public void setUp() {
-        comparator = new Comparator<Integer>() {
 
-            @Override
-            public int compare(final Integer t, final Integer t1) {
-               return t.compareTo(t1);
-            }
-        };
-    }
-    
-  
     @Test
     public void hello() {
         org.junit.Assert.assertTrue("Expected comparison to return less than zero",
-                comparator.compare(i1, i2) < 0);
+                INTEGER_COMPARATOR.compare(i1, i2) < 0);
     }
 }
